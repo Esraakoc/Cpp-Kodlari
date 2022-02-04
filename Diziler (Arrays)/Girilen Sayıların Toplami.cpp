73ce3d7cc84f0f8b53dfc780145380124c87bf55
Esra Koç <202003011070@dogus.edu.tr>
2022-02-04 21:28:41 +0300

    Girilen Sayilarin Toplami.cpp
    Kullanıcının Girdiği n Kadar Sayıların Toplamı

---
+++ Diziler (Arrays)/Girilen Sayıların Toplami.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, toplam = 0;
+	int sayilar[1000];
+	cout << "Eleman sayisi girin=";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "sayilar[" << i << "]=";
+		cin >> sayilar[i];
+		toplam += sayilar[i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << sayilar[i] << "+";
+	}
+	cout << " = " << toplam << endl;
+	system("pause");
+}
