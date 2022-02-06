bfb788c9c09978667b4f5122591ddd4d75529763
Esra Koç <202003011070@dogus.edu.tr>
2022-02-06 23:58:24 +0300

    En-Büyük-Ve-En-Küçük.cpp
    Girilen Sayılardan En Büyük ve En Küçüğü Bulan Kod

---
+++ Diziler (Arrays)/En büyük ve En küçük.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int sayi[1000];
+	int n;
+	cout << "Eleman sayisi girin=";
+	cin >> n;
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "sayi[" << i << "] =";
+		cin >> sayi[i];
+
+	}
+	int ek = sayi[0];
+	int eb = sayi[0];
+	for (int i = 0; i < n; i++)
+	{
+
+		if (ek > sayi[i])
+		{
+			ek = sayi[i];
+		}
+		if (eb < sayi[i])
+		{
+			eb = sayi[i];
+		}
+	}
+	cout << "En buyuk sayi=" << eb << endl;
+	cout << "En kucuk sayi=" << ek << endl;
+
+	system("pause");
+}
