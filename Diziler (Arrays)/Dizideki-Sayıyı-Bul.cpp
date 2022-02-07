a30f920a8bc1d8644298032f7e2bd2255b0c39a8
Esra Koç <202003011070@dogus.edu.tr>
2022-02-07 22:32:34 +0300

    Dizideki-sayiyi-bul.cpp
    Dizinin içinden istediğini sayıyı bul

---
+++ Diziler (Arrays)/Dizideki-Sayıyı-Bul.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a[2][5] = { {3,7,11,9,8},{3,2,5,3,6} };
+	int i, j, s = 0, n;
+	cout << "Hangi sayinin kac tane oldugunu bulmak istersin? ";
+	cin >> n;
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			if (a[i][j] == n)
+			{
+				s++;
+			}
+		}
+	}
+	cout << "Dizide " << s << " tane " << n << " vardir." << endl;
+	return 0;
+}
