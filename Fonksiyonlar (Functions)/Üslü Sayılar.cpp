cdffe45ee5bdd4289c2d14e2e96ad82456a4dbe9
Esra Koç <202003011070@dogus.edu.tr>
2022-02-14 22:56:43 +0300

    Üslü Sayılar.cpp
    İstediğin tabanı ve üssü kullanıcıdan alıp sonucu yazdır

---
+++ Fonksiyonlar (Functions)/Üslü Sayılar.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int taban(int x, int y)
+{
+	double carpim = 1;
+	for (int i = 0; i < y; i++)
+	{
+		carpim = carpim * x;
+	}
+	return carpim;
+}
+
+int main()
+{
+	int h, k;
+	cout << "ilk once bulmak istediginiz taban ardindan ussu giriniz= "; cin >> h >> k;
+	cout << h << "^" << k << " = " << taban(h, k) << endl;
+	int m;
+	cout << "2 nin hangi kuvvetini bulmak istiyorsunuz= ";
+	cin >> m;
+	cout << "2^" << m << " = " << taban(2, m) << endl;
+	cout << "5^3 = " << taban(5, 3) << endl;
+	system("pause");
+}
+
