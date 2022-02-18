8c602bd8fb4b164a37ead60be72a7c43b6a49ab2
Esra Koç <202003011070@dogus.edu.tr>
2022-02-18 12:54:33 +0300

    Tek ve Çift sayı adedi.cpp
    Kullanıcının 10 tane girdiği sayı adedinden tek ve çift sayıların kaç tane olduğunu bul.

---
+++ Fonksiyonlar (Functions)/Tek ve Çift Sayı adedi.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+bool tekCift(int x) //bool değişkeni 1 0 yazıdırır
+{
+	if (x % 2 ==1)
+	{
+		return true; //tekCift() yazınca 1 yazıdırıcak
+	}
+	else
+	{
+		return false; //0 yazdırır
+	}
+}
+
+int main()
+{
+	int sayi, tek = 0, cift =0;
+	for (int i = 1; i <= 10; i++)
+	{
+		cout <<"\t" << i << ". sayiyi giriniz: ";
+		cin >> sayi;
+		tekCift(sayi);
+		if (tekCift(sayi))
+		{
+			tek++;
+		}
+		else
+		{
+			cift++;
+		}
+	}
+	cout << endl << "\t" << "Tek sayi adedi: " << tek << endl;
+	cout << "\t" << "Cift sayi adedi: " << cift << endl;
+
+	return 0;
+}
