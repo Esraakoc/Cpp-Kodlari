d831dfb1365c21400022da4b6290a621431341b9
Esra Koç <202003011070@dogus.edu.tr>
2022-02-20 10:58:52 +0300

    Class-Yapısının-Temel-Gösterimi.cpp
    Class

---
+++ Struct-Class-OOP-Yapıları/Class-Yapısının Temel-Gösterimi.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class student 
+{
+
+public://Herkese açık
+	string name;
+};
+
+int main()
+{
+	student student1;
+	student student2;
+
+	student1.name = "Esra";
+	student2.name = "Arzu";
+
+	cout << "1.ogrencinin ismi: " << student1.name << endl;
+	cout << "2.ogrencinin ismi: " << student2.name << endl;
+
+	return 0;
+}
