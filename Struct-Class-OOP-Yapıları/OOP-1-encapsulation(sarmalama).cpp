8c71dfb3ecfaab4c1d20ff18e5b0f842a490a3b3
Esra Koç <202003011070@dogus.edu.tr>
2022-02-23 18:44:20 +0300

    OOP-1-encapsulation(sarmalama).cpp
    sarmalama

---
+++ Struct-Class-OOP-Yapıları/OOP-1-encapsulation(sarmalama).cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+class ogrenci
+{
+	//Sarmalama ilkesi, bir sınıfa ait değişkenlerin veya niteliklerin ancak
+	//o sınıfa ait metotlar tarafından değiştirilebilmesi ve okunabilmesi ilkesidir.
+	
+private:
+	int notu;
+
+public:
+	string ad;
+	void SetMetot(int a)
+	{
+		if (a <= 100 && a >= 0)
+		{
+			notu = a;
+		}
+		else
+		{
+			cout << "hatali giris" << endl;
+		}
+	}
+	int GetMetot()
+	{
+		return notu;
+	}
+};
+
+
+int main()
+{
+	//OOP Prensipleri  1-encapsulation: sarmalama  2-inheritance: kalıtım(miras alma)  3-polimormphizm: çok biçimlilik
+	ogrenci n1;
+	n1.ad = "Mehmet";
+	n1.SetMetot(90);
+
+	cout << n1.ad << " Isimli ogrencinin notu: " << n1.GetMetot() << endl;
+
+
+	return 0;
+}
