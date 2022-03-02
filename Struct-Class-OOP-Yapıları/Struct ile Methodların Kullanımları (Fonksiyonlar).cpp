3499365ab25cee990b57844201c42cfd4bfe84fb
Esra Koç <202003011070@dogus.edu.tr>
2022-03-02 14:46:26 +0300

    Struct ile Methodların Kullanımları (Fonksiyonlar).cpp
    Methodlar

---
+++ Struct-Class-OOP-Yapıları/Struct ile Methodların Kullanımları (Fonksiyonlar).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct Koordinat
+{
+	int x;
+	int y;
+
+	void merkez()
+	{
+		x = 0;
+		y = 7;
+	}
+};
+int main()
+{
+	Koordinat a;
+	a.x = 5; //x=5 oldu
+	a.y = 3;
+	cout <<"x: "<< a.x << endl; //ekrana 5 değeri yazdırır
+	cout << "y: " << a.y << endl << endl;
+	a.merkez(); //fonksiyonu çağırdık ve x, y değerler aldı
+	cout <<"x: "<< a.x << endl; //ekrana 0 değerii yazdırır
+	cout << "y: " << a.y << endl;
+
+
+	return 0;
+}
