8e5e83e1bdbaad98909f9d6baec29a6928926978
Esra Koç <202003011070@dogus.edu.tr>
2022-03-19 14:47:26 +0300

    Palindrom.cpp
    Palindrom sayısı olup olmadığını öğren

---
+++ Koşullar(Conditions)/Palindrom.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int basamak1, basamak2, basamak4, basamak5, sayi;
+	cout << "---------Palindrom Sayisi Bulma---------" << endl;
+	cout << "5 Basamakli sayi girin: ";
+	cin >> sayi;
+
+	if ((sayi < 100000) && (sayi > 9999))
+	{
+		basamak1 = sayi / 10000;
+		basamak2 = ((sayi % 10000) / 1000);
+		basamak4 = ((((sayi % 10000) % 1000) % 100) / 10);
+		basamak5= ((((sayi % 10000) % 1000) % 100) % 10);
+
+		if ((basamak1 == basamak5) && (basamak2 == basamak4))
+		{
+			cout << "Sayi Palindromdur..." << endl;
+		}
+		else
+		{
+			cout << "Sayi Palindrom degildir!" << endl;
+		}
+	}
+	else
+	{
+		cout << "Girdiginiz sayi 5 basamakli degildir." << endl;
+		return main();
+	}
+
+
+	return 0;
+}
