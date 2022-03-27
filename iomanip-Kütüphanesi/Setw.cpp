d2e6ac98be38f77c2c8d94596cb1840a301cafa0
Esra Koç <202003011070@dogus.edu.tr>
2022-03-27 15:29:57 +0300

    Setw().cpp
    Setw() kullanımı

---
+++ iomanip-Kütüphanesi/Setw.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int sayi=234;
+	string a = "esra";
+
+	cout << setw(9) << sayi << endl; // (9): yazdığımızı satırın başından itibaren 9. sütunda(boşlukta) biter.
+	cout << setw(4) << sayi << endl;
+	cout << setw(1) << sayi << endl;//sayı 3 basamaklı olduğundan dolayı satırın en başından başlıcak sayıyı girmeye.
+	cout << setw(3) << sayi << endl;// (1) ile aynı sütundan başlıcak .
+	cout << setw(6) << a << endl; //--esra 
+	return 0;
+}
