b0f6f57b7b244c6e6543c435db488ac6cfcdda10
Esra Koç <202003011070@dogus.edu.tr>
2022-03-27 16:13:49 +0300

    iomanip-Kütüphanesi
    setprecision fonksiyonu

---
+++ iomanip-Kütüphanesi/Setprecision.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	cout << fixed << setprecision(2);//virgülden sonra sadece 2 basamak yazdırır.
+
+	float sayi, adet, toplam = 0, ortalama;
+
+	cout << "Kac adet sayi gireceksiniz? ";
+	cin >> adet;
+	for (int i = 0; i < adet; i++)
+	{
+		cout << "Sayi giriniz: ";
+		cin >> sayi;
+		toplam += sayi;
+	}
+
+	ortalama = toplam / adet;
+	cout << "Ortalama: " << ortalama << endl;
+	
+	return 0;
+}
