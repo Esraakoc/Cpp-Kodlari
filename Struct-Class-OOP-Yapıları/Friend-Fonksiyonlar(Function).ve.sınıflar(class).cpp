9d7debd368daa2596c8da123bc24ad9247558c08
Esra Koç <202003011070@dogus.edu.tr>
2022-05-07 21:44:32 +0300

    Friend-Fonksiyonlar(Function).ve.sınıflar(class)
    Struct-Class-OOP-Yapıları

---
+++ Struct-Class-OOP-Yapıları/Friend-Fonksiyonlar(Function).ve.sınıflar(class).cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+class Employee
+{
+//özel method
+private:
+	string name;
+	int age;
+	int salary;
+//açık method
+public:
+	Employee(string name, int age, int salary)
+	{
+		//private methoduna erişmek için;
+		this->name = name;
+		this->age = age;
+		this->salary = salary;
+	}
+	/*
+	friend Fonksiyon'u
+	friend void showInfos(Emlpoyee employee); 
+	*/
+	//Friend Class'ı
+	friend class Test;//Prototipini tanıttık
+};
+
+class Test
+{
+public:
+	//Employee'nın private alanlarına erişmeyi sağlar
+	static void showInfos(Employee employee)
+	{
+		cout << employee.name << " " << employee.age << " " << employee.salary << endl;
+	}
+};
+
+//fonksiyonun gövdesi
+/*void showInfos(Employee employee)
+{
+	cout<<employee.name << " " << employee.age << " " << employee.salary << endl;
+}*/
+
+int main()
+{
+	/*
+	Employee employee("Esra", 21, 4000);
+	showInfos(employee);
+	*/
+	Employee employee("Esra", 20, 4000);
+
+	Test::showInfos(employee);
+	return 0;
+}
